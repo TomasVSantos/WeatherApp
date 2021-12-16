5c0763b2ef18d38ff8fb9f9c22d6b0c36ba37732
tomasvsantos <vinhosurvey@gmail.com>
2021-12-16 20:12:58 +0000

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Tomás Santos on 16/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
